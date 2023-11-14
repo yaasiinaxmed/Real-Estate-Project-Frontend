4128b58c77bd8c5a35485c110b17899e3bad1185
yaasiinaxmed <yaskassoy@gmail.com>
2023-11-14 10:57:53 +0300

    done footer

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,8 +1,20 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 
 function Footer() {
+  const [year, setYear] = useState();
+
+  useEffect(() => {
+    const date = new Date();
+    setYear(date.getFullYear());
+  }, [year]);
+
   return (
-    <div>Footer</div>
+    <footer className='w-full bg-primaryColor'>
+      <div className='container p-12 flex flex-col items-center justify-center'>
+        <h2 className='font-medium text-2xl text-white'>Real Estate MarketPlace </h2>
+        <p className='text-xs sm:text-sm text-center mt-9 pb-0 text-white'> © {year} Yaasiin Ahmed - All rigths reserved</p>
+      </div>
+    </footer>
   )
 }
 
