e7c8eec8f33f1e2cc39c5440b952429cb58c6048
yaasiinaxmed <yaskassoy@gmail.com>
2023-11-21 14:59:12 +0300

    update

--- src/store/api/BASE_URL.js
+++ src/store/api/BASE_URL.js
@@ -1,4 +1,4 @@
-// const BASE_URL = "https://real-estate-marketplace-0vnt.onrender.com/api"
-const BASE_URL = "http://localhost:3000/api"
+const BASE_URL = "https://real-estate-marketplace-0vnt.onrender.com/api"
+// const BASE_URL = "http://localhost:3000/api"
 
 export default BASE_URL
